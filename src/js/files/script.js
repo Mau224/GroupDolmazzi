4ee7727d731a562e6061454908a85b436c0cfe35
Mau224 <46903680+Mau224@users.noreply.github.com>
2022-03-17 18:44:40 +0300

    123

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,3 +3,13 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+// let ManifestoItem = document.querySelectorAll('.manifesto__item');
+
+var btns = document.getElementsByClassName("manifesto__item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("mouseenter", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", " active");
+    this.className += " active";
+  });
+}
