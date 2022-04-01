5de529f9a404a3d02cc183589ee95b800e540d11
Mau224 <46903680+Mau224@users.noreply.github.com>
2022-04-02 00:06:19 +0300

    000

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -34,7 +34,7 @@ function initSliders() {
       // для конкретного случая
       modules: [Navigation, EffectCoverflow],
 
-      slidesPerView: 1,
+      slidesPerView: 'auto',
       spaceBetween: 0,
       // autoHeight: true,
       speed: 800,
@@ -84,27 +84,19 @@ function initSliders() {
       },
 
       // Брейкпоинты
-      /*
+
       breakpoints: {
       	320: {
-      		slidesPerView: 1,
+          slidesPerView: 'auto',
       		spaceBetween: 0,
       		autoHeight: true,
       	},
-      	768: {
-      		slidesPerView: 2,
-      		spaceBetween: 20,
-      	},
       	992: {
-      		slidesPerView: 3,
+      		slidesPerView: 'auto',
       		spaceBetween: 20,
-      	},
-      	1268: {
-      		slidesPerView: 4,
-      		spaceBetween: 30,
       	},
       },
-      */
+
       // События
       on: {
 
