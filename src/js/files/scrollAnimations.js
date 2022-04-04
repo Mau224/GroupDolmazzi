e11e139983532fa93bb1ea5be00dacf1de19d59b
Aleksandr Shatokhin <a.shatohin@hh.ru>
2022-04-05 00:08:57 +0300

    changed animations

--- src/js/files/scrollAnimations.js
+++ src/js/files/scrollAnimations.js
@@ -3,36 +3,44 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
+let scrolledPreview = false;
+
 document.addEventListener("DOMContentLoaded", function(event) {
     window.onload = function() {
         window.requestAnimationFrame(function() {
+            window.addEventListener('scroll', () => {
+                if (pageYOffset > 1 && !scrolledPreview) {
+                    scrolledPreview = true;
+                    const tl = gsap.timeline();
+                    tl.to('.wrapper-preview', { y: 0, duration: 0.5 })
+                        .to('.preloader', { display: 'none', delay: 0.5 });
+                    setTimeout(() => {
+                        const prevBlockOffset = document.querySelector('.prev__blok > p').getBoundingClientRect().top;
+                        const prevImgOffset = document.querySelector('.prev__img-wrapper').getBoundingClientRect().top;
+                        const translateImage = prevImgOffset - prevBlockOffset;
 
-            const loaderTimeline = gsap.timeline();
-            loaderTimeline.to('.wrapper-preview', {delay: 0.1})
-                .to('.wrapper-preview', { y: '100%', zIndex: 1002, background: '#fff'})
-                .to('.wrapper-preview', { y: '0%', delay: 2, duration: 1})
-                .to('.preloader', { display: 'none', delay: 1})
-                .to('.wrapper-preview', { zIndex: 1, delay: 1});
+                        const tl = gsap.timeline({
+                            scrollTrigger: {
+                                scrub: true,
+                                pin: true,
+                                pinSpacing: true,
+                                pinReparent: true,
+                                trigger: ".wrapper-preview",
+                            }
+                        });
 
-            const tl = gsap.timeline({ defaults: {ease: "none"} } );
-            const imageAnimation = tl.to(".prev__img", {y: -350, duration: 0.5})
-                .to('.prev__blok p', { opacity: 0 })
-                .to('.prev__img-block h2', { opacity: 1, display: 'flex' })
-                .add('image-upscale')
-                .to('.prev__img-wrapper', { scale: 1.2, duration: 1 }, 'image-scale')
-                .to(".prev__img-block img", { scale: 0.83, duration: 1 }, 'image-scale')
-                .add('image-downscale')
-                .to(".prev__img-wrapper", { scale: 0.6, duration: 1}, 'image-downscale')
-                .to('.prev__main-title', { opacity: 0, display: 'none' }, 'image-downscale')
-                .to('.prev__text-block', { display: 'block', opacity: 1 }, 'image-downscale');
-
-            ScrollTrigger.create({
-                animation: imageAnimation,
-                // markers: true,
-                trigger: ".prev__blok",
-                scroller: '.prev__container',
-                start: "top top",
-                end: "bottom center+=200px",
+                        tl.to(".prev__img", { y: -translateImage })
+                        .to('.prev__blok p', { opacity: 0 })
+                        .to('.prev__img-block h2', { opacity: 1, display: 'flex' })
+                        // .add('image-upscale')
+                        // .to('.prev__img-wrapper', { scale: 1.2 }, 'image-scale')
+                        // .to(".prev__img-block img", { scale: 0.83 }, 'image-scale')
+                        .add('image-downscale')
+                        .to(".prev__img-wrapper", { scale: 0.6 }, 'image-downscale')
+                        .to('.prev__main-title', { opacity: 0, display: 'none' }, 'image-downscale')
+                        .to('.prev__text-block', { display: 'block', opacity: 1 }, 'image-downscale');
+                    }, 500);
+                }
             });
         });
 
@@ -40,7 +48,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 });
 document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelector('.btn-map').addEventListener('click', () => {
-    document.querySelector('.prev__map').classList.add('active');
+  document.querySelector('.btn-map').addEventListener('click', (e) => {
+      e.preventDefault();
+      gsap.to('body', { overflow: 'hidden' })
+      document.querySelector('.prev__map').classList.add('active');
   });
 });
