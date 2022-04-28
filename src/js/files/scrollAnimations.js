4b810886e7de0ed7e3b0a5e4a318f059e17a9c26
Aleksandr Shatokhin <a.shatohin@hh.ru>
2022-04-29 01:59:47 +0300

    update animation

--- src/js/files/scrollAnimations.js
+++ src/js/files/scrollAnimations.js
@@ -3,25 +3,56 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
+(function() {
+    const throttle = function(type, name, obj) {
+        obj = obj || window;
+        let running = false;
+        const func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(() => {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    throttle('resize', 'optimizedResize');
+})();
+
 let scrolledPreview = false;
 
 document.addEventListener("DOMContentLoaded", function(event) {
     window.onload = function() {
         const wrapperPreview = document.querySelector('.wrapper-preview');
+        const prevBlock = document.querySelector('.prev__blok > p');
+        const prevImg = document.querySelector('.prev__img-wrapper');
+        let prevBlockOffset = prevBlock.getBoundingClientRect().top;
+        let prevImgOffset = prevImg.getBoundingClientRect().top;
+        let translateImage = prevImgOffset - prevBlockOffset;
+
+        window.addEventListener('optimizedResize', () => {
+            prevBlockOffset = prevBlock.getBoundingClientRect().top;
+            prevImgOffset = prevImg.getBoundingClientRect().top;
+            translateImage = prevImgOffset - prevBlockOffset;
+        });
+
 
         if (wrapperPreview) {
-            window.requestAnimationFrame(function() {
-                window.addEventListener('scroll', () => {
-                    if (pageYOffset > 1 && !scrolledPreview) {
-                        scrolledPreview = true;
-                        const tl = gsap.timeline();
-                        tl.to('.wrapper-preview', { y: 0, duration: 0.5 })
-                            .to('.preloader', { display: 'none', delay: 0.5 });
-                        setTimeout(() => {
-                            const prevBlockOffset = document.querySelector('.prev__blok > p').getBoundingClientRect().top;
-                            const prevImgOffset = document.querySelector('.prev__img-wrapper').getBoundingClientRect().top;
-                            const translateImage = prevImgOffset - prevBlockOffset;
+            if (history.scrollRestoration) {
+                history.scrollRestoration = 'manual';
+            } else {
+                window.onbeforeunload = function () {
+                    window.scrollTo(0, 0);
+                }
+            }
 
+            window.requestAnimationFrame(function() {
+                window.addEventListener('wheel', (e) => {
+                    if (e.deltaY > 1 && !scrolledPreview) {
+                        scrolledPreview = true;
+                        const scrollFunction = () => {
                             const tl = gsap.timeline({
                                 scrollTrigger: {
                                     scrub: true,
@@ -33,16 +64,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
                             });
 
                             tl.to(".prev__img", { y: -translateImage })
-                                .to('.prev__blok p', { opacity: 0 })
-                                .to('.prev__img-block h2', { opacity: 1, display: 'flex' })
-                                // .add('image-upscale')
-                                // .to('.prev__img-wrapper', { scale: 1.2 }, 'image-scale')
-                                // .to(".prev__img-block img", { scale: 0.83 }, 'image-scale')
+                                .to('.prev__blok p', { opacity: 0, duration: 0})
                                 .add('image-downscale')
                                 .to(".prev__img-wrapper", { scale: 0.6 }, 'image-downscale')
                                 .to('.prev__main-title', { opacity: 0, display: 'none' }, 'image-downscale')
                                 .to('.prev__text-block', { display: 'block', opacity: 1 }, 'image-downscale');
-                        }, 500);
+                        }
+                        const tl = gsap.timeline({ onComplete: scrollFunction });
+                        tl.to('.wrapper-preview', { y: 0, duration: 0.5 })
+                            .to('.preloader', { display: 'none' });
                     }
                 });
             });
