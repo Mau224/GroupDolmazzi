0730a40905476f46490dec751469120153ee23ab
Aleksandr Shatokhin <a.shatohin@hh.ru>
2022-05-26 19:45:41 +0300

    fix video

--- src/js/files/videoScroll.js
+++ src/js/files/videoScroll.js
@@ -4,28 +4,41 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 gsap.registerPlugin(ScrollTrigger);
 
 const image = document.querySelector(".video-background") || {};
+const imageSource = document.querySelector('.video__wrap > picture > source') || {};
 
 const frameCount = 148;
 const currentFrame = index => (
-    `/img/video/Full_5_1${index.toString().padStart(3, '0')}.jpg`
+    `img/video/Full_5_1${index.toString().padStart(3, '0')}.jpg`
+)
+const currentFrameWebP = index => (
+    `img/video/Full_5_1${index.toString().padStart(3, '0')}.webp`
 )
 
 const preloadImages = () => {
     for (let i = 0; i <= frameCount; i++) {
         const img = new Image();
+        const source = document.createElement("source");
         img.src = currentFrame(i);
+        source.srcset = currentFrameWebP(i);
     }
 };
 
 const img = new Image()
+const imgSource = document.createElement("source");
 img.src = currentFrame(1);
+imgSource.srcset = currentFrameWebP(1);
 img.onload = () => {
     image.src = img && img?.src;
+}
+imgSource.onload = () => {
+    imageSource.srcset = imgSource && imgSource?.srcset;
 }
 
 const updateImage = index => {
     img.src = currentFrame(index);
+    imgSource.srcset = currentFrameWebP(index);
     image.src = img?.src;
+    imageSource.srcset = imgSource?.srcset;
 }
 
 const scrollVideo = (self) => {
