8f69526dfc5eb26fc6db2ac2378fa5cd17be9b86
Aleksandr Shatokhin <a.shatohin@hh.ru>
2022-07-28 14:20:30 +0300

    update player and change animation speed

--- src/js/files/players.js
+++ src/js/files/players.js
@@ -1,10 +1,26 @@
-const audioPlayer = function(element) {
+let players = {};
+
+const pauseOtherPlayers = () => {
+    for (let [key, value] of Object.entries(players)) {
+        if (value) {
+            const player = document.querySelector(`[data-id="${key}"]`);
+            const playButton = player.querySelector(key.startsWith('audio') ? '.play-icon' : '.video-play-icon');
+
+            playButton.click();
+        }
+    }
+}
+
+const audioPlayer = (element, index) => {
+    const id = `audio-${index}`;
     const playIconContainer = element.querySelector('.play-icon');
     const nextIconContainer = element.querySelector('.next-icon');
     const previousIconContainer = element.querySelector('.previous-icon');
     const seekSlider = element.querySelector('.seek-slider');
     const audioTitle = element.querySelector('.audio-title');
     let playState = 'play';
+
+    element.setAttribute('data-id', id);
 
     const audio = element.querySelector('.audio');
     const durationContainer = element.querySelector('.duration');
@@ -62,15 +78,18 @@ const audioPlayer = function(element) {
 
         playIconContainer.addEventListener('click', () => {
             if(playState === 'play') {
+                pauseOtherPlayers();
                 audio.play();
                 playIconContainer.classList.add('active');
                 requestAnimationFrame(whilePlaying);
                 playState = 'pause';
+                players[id] = true;
             } else {
                 audio.pause();
                 cancelAnimationFrame(raf);
                 playState = 'play';
                 playIconContainer.classList.remove('active');
+                players[id] = false;
             }
         });
 
@@ -117,10 +136,87 @@ const audioPlayer = function(element) {
             audio.currentTime = e.target.value;
         })
     }
-}
+};
 
-let instance = document.querySelectorAll('.audio-player-container');
+const videoPlayer = (element, index) => {
+    const id = `video-${index}`;
+    const playIconContainer = element.querySelector('.video-play-icon');
+    const seekSlider = element.querySelector('.video-seek-slider');
+    const video = element.querySelector('video');
+    const durationContainer = element.querySelector('.video-duration');
+    const currentTimeContainer = element.querySelector('.video-current-time');
+    let playState = 'play';
+    let raf = null;
 
-instance.forEach((element) => {
-    audioPlayer(element);
+    element.setAttribute('data-id', id);
+
+    if (playIconContainer &&
+        seekSlider &&
+        video &&
+        durationContainer &&
+        currentTimeContainer) {
+
+        playIconContainer.addEventListener('click', () => {
+            if(playState === 'play') {
+                pauseOtherPlayers();
+                video.play();
+                playIconContainer.classList.add('active');
+                requestAnimationFrame(whilePlaying);
+                playState = 'pause';
+                players[id] = true;
+            } else {
+                video.pause();
+                playIconContainer.classList.remove('active')
+                cancelAnimationFrame(raf);
+                playState = 'play';
+                players[id] = false;
+            }
+        });
+
+        const calculateTime = (secs) => {
+            const minutes = Math.floor(secs / 60);
+            const seconds = Math.floor(secs % 60);
+            const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+            return `${minutes}:${returnedSeconds}`;
+        }
+
+        const displayDuration = () => {
+            durationContainer.textContent = calculateTime(video.duration);
+        }
+
+        const setSliderMax = () => {
+            seekSlider.max = Math.floor(video.duration);
+        }
+
+        const whilePlaying = () => {
+            seekSlider.value = Math.floor(video.currentTime);
+            currentTimeContainer.textContent = calculateTime(seekSlider.value);
+            raf = requestAnimationFrame(whilePlaying);
+        }
+
+        if (video.readyState > 0) {
+            displayDuration();
+            setSliderMax();
+        } else {
+            video.addEventListener('loadedmetadata', () => {
+                displayDuration();
+                setSliderMax();
+            });
+        }
+
+        seekSlider.addEventListener('input', (e) => {
+            video.currentTime = e.target.value;
+        })
+    }
+};
+
+let audioInstance = document.querySelectorAll('.audio-player-container');
+let videoInstance = document.querySelectorAll('.video-player-container');
+
+audioInstance.forEach((element, index) => {
+    audioPlayer(element, index);
+})
+
+videoInstance.forEach((element, index) => {
+    videoPlayer(element, index);
 })
